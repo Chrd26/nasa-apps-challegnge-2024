4bd1d72e9cc566a9dab373a485b1d15ddaa5fd62
Chrd26 <christosdouris@outlook.com>
2024-10-06 22:41:23 +0300

    added some more info. That is for now, I need to create a demo

--- controllers/results.py
+++ controllers/results.py
@@ -1,8 +1,10 @@
+
 from flask import Flask
 from flask import render_template, request
 import openeo
 import os
 from geopy.geocoders import Nominatim
+from openeo.processes import ProcessBuilder
 
 class Results:
     def __init__(self, location):
@@ -19,31 +21,38 @@ class Results:
             return
 
         print(location.latitude, location.longitude)
-        width = location.longitude + 0.6
-        height =location.latitude + 0.3
+        width = location.longitude + 0.3
+        height =location.latitude + 0.15
 
         connection = openeo.connect("openeo.dataspace.copernicus.eu")
         getConnections = connection.list_collection_ids()
         print(getConnections)
 
-        connection.describe_collection("COPERNICUS_PLANT_PHENOLOGY_INDEX")
+        # band combination sentinel 2
+        # https://gisgeography.com/sentinel-2-bands-combinations/
+
+        connection.describe_collection("SENTINEL2_L2A")
         connection.authenticate_oidc()
         datacube = connection.load_collection(
-        "COPERNICUS_PLANT_PHENOLOGY_INDEX",
+        "SENTINEL2_L2A",
         spatial_extent={"west": location.longitude, 
                         "south": location.latitude, 
                         "east": width, "north": height},
         temporal_extent = ["2021-02-01", "2021-04-30"],
-        bands=["QFLAG", "PPI"])
+        bands=["B11", "B02", "B08"],
+        max_cloud_cover = 20)
 
-        ppi = datacube.band("PPI") * 0.0001
-        qflag = datacube.band("QFLAG") * 0.0001
+        swir1 = datacube.band("B11") * 0.0001
+        nir = datacube.band("B08") * 0.0001
+        blue = datacube.band("B02") * 0.0001
 
-        evi_cube = ppi + qflag
-
+        evi_cube = (nir - swir1) / (nir + swir1 * blue)
         evi_composite = evi_cube.max_time()
+
         evi_composite.download("./static/result.tiff")
         # evi_composite.execute_batch()
 
     def DeletePicture(self):
         os.remove("./static/result.tiff")
+
+
